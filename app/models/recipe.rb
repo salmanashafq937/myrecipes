967b0188821d0bae37f6ee92c3e1fee06e6f9d6a
Zeryab Khan <zeryabkhan91@gmail.com>
2019-08-06 17:08:21 +0500

    add one-many association between Chef and Recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,9 @@ class Recipe < ApplicationRecord
  validates :name, presence: true, length: {maximum: 50}
  validates :description, presence: true, length: {minimum: 5, maximum: 500}
 
+ belongs_to :chef
+ validates :chef_id, presence: true
+
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_100017) do
+ActiveRecord::Schema.define(version: 2019_08_06_111024) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_08_06_100017) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "chef_id"
   end
 
 end
